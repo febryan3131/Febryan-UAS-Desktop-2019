e044b52585b8bf699730bda140d43b3fc02f6d7a
febryan3131 <febryan19pemangkat@gmail.com>
2019-05-22 04:31:48 +0700

    bnb

---
+++ Febryan-UAS-Desktop-2109/src/febryan/uas/desktop/pkg2109/InputTransaksi.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package febryan.uas.desktop.pkg2109;
+
+/**
+ *
+ * @author febryan
+ */
+class InputTransaksi {
+    
+}
